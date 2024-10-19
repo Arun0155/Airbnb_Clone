e8c6306efb9183bfbcaa87d79993cc93ecf2dd43
Arun0155 <arunsharmam99@gmail.com>
2024-10-19 14:57:09 +0530

    first commit

--- routes/listing.js
+++ routes/listing.js
@@ -24,7 +24,12 @@ router.get("/", wrapAsync( async (req,res) => {
 
 
 // New Route
-router.get("//new", (req,res) => {
+router.get("/new", (req,res) => {
+    console.log(req.user);
+    if(!req.isAuthenticated()) {
+        req.flash("error", "you must be logged in to create listing!");
+        return res.redirect("/login");
+    }
     res.render("listings/new.ejs");
 });
 
@@ -32,6 +37,10 @@ router.get("//new", (req,res) => {
 router.get("/:id" , wrapAsync( async (req, res) => {
     let {id} =req.params;
     const listing = await Listing.findById(id).populate("reviews");
+    if(!listing) {
+        req.flash("error", "Listing you requested for dose not exist");
+        res.redirect("/listings");
+    }
     res.render("listings/show.ejs",{listing});
 })) ;
 
@@ -42,6 +51,7 @@ router.post(
     wrapAsync(async (req, res, next) => {
         const newListing = new Listing(req.body.listing);
         await newListing.save();
+        req.flash("success", "New Listing Created!");
         res.redirect("/listings");
     })
 );
@@ -50,6 +60,10 @@ router.post(
 router.get("/:id/edit", wrapAsync( async (req,res) => {
     let {id} =req.params;
     const listing = await Listing.findById(id);
+    if(!listing) {
+        req.flash("error", "Listing you requested for dose not exist");
+        res.redirect("/listings");
+    }
     res.render("listings/edit.ejs", {listing});
 }));
 
@@ -60,6 +74,7 @@ router.put(
     wrapAsync( async (req,res) => {
     let { id } = req.params;
     await Listing.findByIdAndUpdate (id, {...req.body.listing});
+    req.flash("success", "Listing Updated");
     res.redirect(`/listings/${id}`);
 }));
 
@@ -68,6 +83,7 @@ router.delete("/:id", wrapAsync( async (req,res) => {
     let { id } =req.params;
     let deletedListing = await Listing.findByIdAndDelete(id);
     console.log(deletedListing);
+    req.flash("success", "Listing Deleted");
     res.redirect("/listings");
 }));
 
