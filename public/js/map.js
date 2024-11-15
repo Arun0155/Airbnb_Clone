468e792859d899178f81d11479861a953bd01ac5
Arun0155 <arunsharmam99@gmail.com>
2024-11-16 00:31:09 +0530

    add

--- public/js/map.js
+++ public/js/map.js
@@ -3,11 +3,16 @@ mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
     container: 'map', // container ID
     style: "mapbox://styles/mapbox/streets-v12",
-    center: coordinates, // starting position [lng, lat]. Note that lat must be set between -90 and 90
-    zoom: 9, // starting zoom
+    center: listing.geometery.coordinates, // starting position [lng, lat]. Note that lat must be set between -90 and 90
+    zoom: 8, // starting zoom
 });
 
 
-const marker1 = new mapboxgl.Marker()
-.setLngLat(coordinates)
+const marker1 = new mapboxgl.Marker({ color: "red" })
+.setLngLat(listing.geometery.coordinates)
+.setPopup(
+    new mapboxgl.Popup({ offset: 25 }).setHTML(
+        `<h4></h4>${listing.title}<p>Exact Location provided after booking</p>`
+    )
+)
 .addTo(map);
